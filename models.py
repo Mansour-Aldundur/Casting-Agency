5d13b395be037c7f89d42bd5ec61991cb26a33b5
Mansour Aldundur <mansouraldundur@gmail.com>
2020-06-28 01:35:56 +0300

    Chore(local): Creating the models

--- models.py
+++ models.py
@@ -1,1 +1,88 @@
 
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+database_path = 'postgres://postgres@localhost:5432/casting_agency'
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+Binds a flask application to an SQLAlchemy instance
+'''
+
+
+def setup_db(app):
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_path
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+
+'''
+Movie
+defines a movie that has a title and a release date
+'''
+
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    release_date = db.Column(db.DateTime(), nullable=False)
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    @property
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date
+        }
+
+
+'''
+Actor
+defines an actor who has a name, age and gender
+'''
+
+
+class Actor(db.Model):
+    __tablename__ = 'actors'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    gender = db.Column(db.String, nullable=False)
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    @property
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender
+        }
